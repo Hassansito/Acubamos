8a6e510f7d9f528b6009b84ba7c82d35d5aaa5db
dlazo2023 <dariellazo97@gmail.com>
2025-01-24 08:56:16 +0100

    sidebar items fixed

--- src/layouts/default-layout/config/MainMenuConfig.ts
+++ src/layouts/default-layout/config/MainMenuConfig.ts
@@ -9,18 +9,18 @@ const MainMenuConfig: Array<MenuItem> = [
         keenthemesIcon: "element-11",
         bootstrapIcon: "bi-app-indicator",
       },
-      {
-        heading: "layoutBuilder",
-        route: "/builder",
-        keenthemesIcon: "switch",
-        bootstrapIcon: "bi-layers",
-      },
+      // {
+      //   heading: "layoutBuilder",
+      //   route: "/builder",
+      //   keenthemesIcon: "switch",
+      //   bootstrapIcon: "bi-layers",
+      // },
     ],
   },
 
   /*               Administración            */
   {
-    heading: "adminintracion",
+    heading: "Adminintración",
     route: "/adminintracion",
     pages: [
       {
@@ -102,152 +102,34 @@ const MainMenuConfig: Array<MenuItem> = [
         bootstrapIcon: "bi-shield-check",
       },
       {
-        sectionTitle: "perfil",
-        route: "/perfil",
-        keenthemesIcon: "element-7",
-        bootstrapIcon: "bi-layers",
-      },
-    ],
-  },
-  /*               Comercialización            */
-  {
-    heading: "comercialización",
-    route: "/comercializacion",
-    pages: [
-      {
-        heading: "reservas",
-        route: "/comercializacion/reservas",
-        keenthemesIcon: "fingerprint-scanning",
-        bootstrapIcon: "bi-sticky",
-      },
-      {
-        heading: "contratos",
-        route: "/comercializacion/contratos",
-        keenthemesIcon: "fingerprint-scanning",
-        bootstrapIcon: "bi-sticky",
-      },
-      {
-        heading: "ofertas",
-        route: "/comercializacion/ofertas",
-        keenthemesIcon: "fingerprint-scanning",
-        bootstrapIcon: "bi-sticky",
-      },
-      {
-        heading: "productos",
-        route: "/comercializacion/productos",
-        keenthemesIcon: "fingerprint-scanning",
-        bootstrapIcon: "bi-sticky",
-      },
-    ],
-  },
-  /*               Pages            */
-  {
-    heading: "Pages",
-    route: "/pages",
-    pages: [
-      {
-        heading: "perfil",
-        route: "/pages/perfil",
-        keenthemesIcon: "fingerprint-scanning",
-        bootstrapIcon: "bi-sticky",
-      },
-      {
-        heading: "ecomerce",
-        route: "/pages/ecomerce",
-        keenthemesIcon: "fingerprint-scanning",
-        bootstrapIcon: "bi-sticky",
-      },
-      {
-        heading: "files",
-        route: "/pages/files",
-        keenthemesIcon: "fingerprint-scanning",
-        bootstrapIcon: "bi-sticky",
-      },
-      {
-        heading: "correo",
-        route: "/pages/correo",
-        keenthemesIcon: "fingerprint-scanning",
-        bootstrapIcon: "bi-sticky",
-      },
-      {
-        heading: "calendario",
-        route: "/pages/calendario",
-        keenthemesIcon: "fingerprint-scanning",
-        bootstrapIcon: "bi-sticky",
-      },
-      {
-        heading: "contactos",
-        route: "/pages/contactos",
-        keenthemesIcon: "fingerprint-scanning",
-        bootstrapIcon: "bi-sticky",
-      },
-      {
-        heading: "customers",
-        route: "/pages/customers",
-        keenthemesIcon: "fingerprint-scanning",
-        bootstrapIcon: "bi-sticky",
-      },
-      {
-        heading: "facturas",
-        route: "/pages/facturas",
-        keenthemesIcon: "fingerprint-scanning",
-        bootstrapIcon: "bi-sticky",
-      },
-    ],
-  },
-  {
-    heading: "craft",
-    route: "/crafted",
-    pages: [
-      {
-        sectionTitle: "pages",
-        route: "/pages",
-        keenthemesIcon: "element-plus",
-        bootstrapIcon: "bi-archive",
+        sectionTitle: "profile",
+        route: "/profile",
+        keenthemesIcon: "design",
+        bootstrapIcon: "bi-shield-check",
         sub: [
           {
-            sectionTitle: "profile",
-            route: "/profile",
-            sub: [
-              {
-                heading: "profileOverview",
-                route: "/crafted/pages/profile/overview",
-              },
-              {
-                heading: "projects",
-                route: "/crafted/pages/profile/projects",
-              },
-              {
-                heading: "campaigns",
-                route: "/crafted/pages/profile/campaigns",
-              },
-              {
-                heading: "documents",
-                route: "/crafted/pages/profile/documents",
-              },
-              {
-                heading: "connections",
-                route: "/crafted/pages/profile/connections",
-              },
-              {
-                heading: "activity",
-                route: "/crafted/pages/profile/activity",
-              },
-            ],
+            heading: "profileOverview",
+            route: "/crafted/pages/profile/overview",
           },
           {
-            sectionTitle: "wizards",
-            route: "/wizard",
-            sub: [
-              {
-                heading: "horizontal",
-                route: "/crafted/pages/wizards/horizontal",
-              },
-              {
-                heading: "vertical",
-                route: "/crafted/pages/wizards/vertical",
-              },
-            ],
+            heading: "projects",
+            route: "/crafted/pages/profile/projects",
+          },
+          {
+            heading: "campaigns",
+            route: "/crafted/pages/profile/campaigns",
+          },
+          {
+            heading: "documents",
+            route: "/crafted/pages/profile/documents",
+          },
+          {
+            heading: "connections",
+            route: "/crafted/pages/profile/connections",
+          },
+          {
+            heading: "activity",
+            route: "/crafted/pages/profile/activity",
           },
         ],
       },
@@ -267,150 +149,6 @@ const MainMenuConfig: Array<MenuItem> = [
           },
         ],
       },
-      {
-        sectionTitle: "authentication",
-        keenthemesIcon: "fingerprint-scanning",
-        bootstrapIcon: "bi-sticky",
-        sub: [
-          {
-            sectionTitle: "basicFlow",
-            sub: [
-              {
-                heading: "signIn",
-                route: "/sign-in",
-              },
-              {
-                heading: "signUp",
-                route: "/sign-up",
-              },
-              {
-                heading: "passwordReset",
-                route: "/password-reset",
-              },
-            ],
-          },
-          {
-            heading: "multiStepSignUp",
-            route: "/multi-step-sign-up",
-          },
-          {
-            heading: "error404",
-            route: "/404",
-          },
-          {
-            heading: "error500",
-            route: "/500",
-          },
-        ],
-      },
-      {
-        sectionTitle: "modals",
-        route: "/modals",
-        keenthemesIcon: "design",
-        bootstrapIcon: "bi-shield-check",
-        sub: [
-          {
-            sectionTitle: "general",
-            route: "/general",
-            sub: [
-              {
-                heading: "inviteFriends",
-                route: "/crafted/modals/general/invite-friends",
-              },
-              {
-                heading: "viewUsers",
-                route: "/crafted/modals/general/view-user",
-              },
-              {
-                heading: "upgradePlan",
-                route: "/crafted/modals/general/upgrade-plan",
-              },
-              {
-                heading: "shareAndEarn",
-                route: "/crafted/modals/general/share-and-earn",
-              },
-            ],
-          },
-          {
-            sectionTitle: "forms",
-            route: "/forms",
-            sub: [
-              {
-                heading: "newTarget",
-                route: "/crafted/modals/forms/new-target",
-              },
-              {
-                heading: "newCard",
-                route: "/crafted/modals/forms/new-card",
-              },
-              {
-                heading: "newAddress",
-                route: "/crafted/modals/forms/new-address",
-              },
-              {
-                heading: "createAPIKey",
-                route: "/crafted/modals/forms/create-api-key",
-              },
-            ],
-          },
-          {
-            sectionTitle: "wizards",
-            route: "/wizards",
-            sub: [
-              {
-                heading: "twoFactorAuth",
-                route: "/crafted/modals/wizards/two-factor-auth",
-              },
-              {
-                heading: "createApp",
-                route: "/crafted/modals/wizards/create-app",
-              },
-              {
-                heading: "createAccount",
-                route: "/crafted/modals/wizards/create-account",
-              },
-            ],
-          },
-        ],
-      },
-      {
-        sectionTitle: "widgets",
-        route: "/widgets",
-        keenthemesIcon: "element-7",
-        bootstrapIcon: "bi-layers",
-        sub: [
-          {
-            heading: "widgetsLists",
-            route: "/crafted/widgets/lists",
-          },
-          {
-            heading: "widgetsStatistics",
-            route: "/crafted/widgets/statistics",
-          },
-          {
-            heading: "widgetsCharts",
-            route: "/crafted/widgets/charts",
-          },
-          {
-            heading: "widgetsMixed",
-            route: "/crafted/widgets/mixed",
-          },
-          {
-            heading: "widgetsTables",
-            route: "/crafted/widgets/tables",
-          },
-          {
-            heading: "widgetsFeeds",
-            route: "/crafted/widgets/feeds",
-          },
-        ],
-      },
-    ],
-  },
-  {
-    heading: "apps",
-    route: "/apps",
-    pages: [
       {
         sectionTitle: "customers",
         route: "/customers",
@@ -455,31 +193,91 @@ const MainMenuConfig: Array<MenuItem> = [
           },
         ],
       },
+    ],
+  },
+  /*               Comercialización            */
+  {
+    heading: "Comercialización",
+    route: "/comercializacion",
+    pages: [
+      {
+        heading: "reservas",
+        route: "/comercializacion/reservas",
+        keenthemesIcon: "fingerprint-scanning",
+        bootstrapIcon: "bi-sticky",
+      },
+      {
+        heading: "contratos",
+        route: "/comercializacion/contratos",
+        keenthemesIcon: "fingerprint-scanning",
+        bootstrapIcon: "bi-sticky",
+      },
+      {
+        heading: "ofertas",
+        route: "/comercializacion/ofertas",
+        keenthemesIcon: "fingerprint-scanning",
+        bootstrapIcon: "bi-sticky",
+      },
+      {
+        heading: "productos",
+        route: "/comercializacion/productos",
+        keenthemesIcon: "fingerprint-scanning",
+        bootstrapIcon: "bi-sticky",
+      },
       {
         heading: "calendarApp",
         route: "/apps/calendar",
         keenthemesIcon: "calendar-8",
         bootstrapIcon: "bi-calendar3-event",
       },
+    ],
+  },
+  /*               Pages            */
+  {
+    heading: "Páginas",
+    route: "/pages",
+    pages: [
       {
-        sectionTitle: "chat",
-        route: "/chat",
-        keenthemesIcon: "message-text-2",
-        bootstrapIcon: "bi-chat-left",
-        sub: [
-          {
-            heading: "privateChat",
-            route: "/apps/chat/private-chat",
-          },
-          {
-            heading: "groupChat",
-            route: "/apps/chat/group-chat",
-          },
-          {
-            heading: "drawerChat",
-            route: "/apps/chat/drawer-chat",
-          },
-        ],
+        heading: "ecomerce",
+        route: "/pages/ecomerce",
+        keenthemesIcon: "fingerprint-scanning",
+        bootstrapIcon: "bi-sticky",
+      },
+      {
+        heading: "files",
+        route: "/pages/files",
+        keenthemesIcon: "fingerprint-scanning",
+        bootstrapIcon: "bi-sticky",
+      },
+      {
+        heading: "correo",
+        route: "/pages/correo",
+        keenthemesIcon: "fingerprint-scanning",
+        bootstrapIcon: "bi-sticky",
+      },
+      {
+        heading: "calendario",
+        route: "/pages/calendario",
+        keenthemesIcon: "fingerprint-scanning",
+        bootstrapIcon: "bi-sticky",
+      },
+      {
+        heading: "contactos",
+        route: "/pages/contactos",
+        keenthemesIcon: "fingerprint-scanning",
+        bootstrapIcon: "bi-sticky",
+      },
+      {
+        heading: "customers",
+        route: "/pages/customers",
+        keenthemesIcon: "fingerprint-scanning",
+        bootstrapIcon: "bi-sticky",
+      },
+      {
+        heading: "facturas",
+        route: "/pages/facturas",
+        keenthemesIcon: "fingerprint-scanning",
+        bootstrapIcon: "bi-sticky",
       },
     ],
   },
