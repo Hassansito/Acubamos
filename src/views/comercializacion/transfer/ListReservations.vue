537c0d97e38238467b1d268a45ed483e90cf9eb0
Hassansito <108918273+Hassansito@users.noreply.github.com>
2025-02-13 01:19:04 -0800

    Update ListReservations.vue

--- src/views/comercializacion/transfer/ListReservations.vue
+++ src/views/comercializacion/transfer/ListReservations.vue
@@ -396,7 +396,7 @@ export default defineComponent({
             const year = date.getFullYear();
             const month = String(date.getMonth() + 1).padStart(2, '0');
             const day = String(date.getUTCDate()).padStart(2, '0');
-            return `${year}/${month}/${day}`;
+            return `${day}/${month}/${year}`;
         };
         return {
             tableHeader,
