276c6f9da85490737b72a441ede0465e8b7b638e
Hassansito <108918273+Hassansito@users.noreply.github.com>
2025-02-17 03:08:24 -0500

    Update

--- src/stores/reservas.ts
+++ src/stores/reservas.ts
@@ -12,6 +12,16 @@ export const useReservasStore = defineStore("reservas", {
       this.reservas.push(product);
       console.log("Productos en el store:", this.reservas); 
     },
+    
+    updateReserva(updateReserva: IReservaciones) {
+      const index = this.reservas.findIndex((o) => o.id === updateReserva.id);
+      if (index !== -1) {
+        this.reservas[index] = updateReserva;
+        console.log("Oferta actualizada en el store:", updateReserva);
+      } else {
+        console.error("Oferta no encontrada para actualizar");
+      }
+    },
   },
   getters: {
     getProducts: (state) => state.reservas,
